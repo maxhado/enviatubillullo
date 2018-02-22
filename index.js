388e1175cbc386b2be3445e1003d5768a72e3b80
Jesus <maxhado1993@gmail.com>
2018-02-22 08:22:39 -0400

    0.0.6

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.get('/', function (req, res) {
 
 const port = process.env.PORT || 3000;
 
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mojmachs:231193j@ds143678.mlab.com:43678/heroku_83f133dc');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
+
+
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
