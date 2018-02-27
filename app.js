da68b6a507a105825941b5a7ee60b3bed4dfe0cc
Jesus <maxhado1993@gmail.com>
2018-02-26 21:10:59 -0400

    prueba

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const app = express();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-const routes = require('./routes/index');
+var book = require('./routes/book');
+var app = express();
 
-//Configuraciones
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://mojmachs:231193j@ds243768.mlab.com:43768/heroku_077w30sf', { useMongoClient: true, promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine', 'ejs');
-
-//middeware
-app.use((req,res, next) =>{
-	console.log(req.url, req.method);
-	next();
-});
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('view engine', 'html');
+app.use('/books', express.static(path.join(__dirname, 'dist')));
+app.use('/book', book);
 
-
-
-//archivos estaticos
-
-app.use(express.static("public"));
-
-//rutas
-app.use(routes);
-
-
-//errores
-
-
-
-//servidor
-app.listen(app.get('port'), () => {
-	console.log('server on port ', app.get('port'));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
